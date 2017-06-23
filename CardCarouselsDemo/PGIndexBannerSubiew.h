a7606b4d0f68ba4e8aa70be210511cd464b80f38
lixianjun <lixianjun@chengyiwm.com>
2017-06-23 01:27:54 -0700

    卡片轮播效果

---
+++ CardCarouselsDemo/PGIndexBannerSubiew.h
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  CardCarouselsDemo
+//
+//  Created by lixianjun on 2017/6/23.
+//  Copyright © 2017年 lxj. All rights reserved.
+//
+
+
+
+#import <UIKit/UIKit.h>
+
+@interface PGIndexBannerSubiew : UIView
+
+/**
+ *  主图
+ */
+@property (nonatomic, strong) UIImageView *mainImageView;
+
+/**
+ *  用来变色的view
+ */
+@property (nonatomic, strong) UIView *coverView;
+
+@end
---
+++ CardCarouselsDemo/PGIndexBannerSubiew.m
@@ -0,0 +1,43 @@
+//
+//  ViewController.m
+//  CardCarouselsDemo
+//
+//  Created by lixianjun on 2017/6/23.
+//  Copyright © 2017年 lxj. All rights reserved.
+//
+
+#import "PGIndexBannerSubiew.h"
+
+@implementation PGIndexBannerSubiew
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    
+    self = [super initWithFrame:frame];
+    
+    if (self) {
+        
+        [self addSubview:self.mainImageView];
+        [self addSubview:self.coverView];
+    }
+    
+    return self;
+}
+
+- (UIImageView *)mainImageView {
+    
+    if (_mainImageView == nil) {
+        _mainImageView = [[UIImageView alloc] initWithFrame:self.bounds];
+        _mainImageView.userInteractionEnabled = YES;
+    }
+    return _mainImageView;
+}
+
+- (UIView *)coverView {
+    if (_coverView == nil) {
+        _coverView = [[UIView alloc] initWithFrame:self.bounds];
+        _coverView.backgroundColor = [UIColor blackColor];
+    }
+    return _coverView;
+}
+
+@end
